1c024f5e4426fe46c5d9a6ee3c4f31e04a04b142
seba13h <sebastian.rodriguezb@sansano.usm.cl>
2018-04-15 19:22:10 -0300

    add new proyect angular

---
+++ scaffolding web con yeo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scaffoldingWebConYeoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scaffoldingWebConYeoApp
+ */
+angular.module('scaffoldingWebConYeoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
